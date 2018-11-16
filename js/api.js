d6e22f377b9551bd1dadd4a0a9c7041b2dc7821c
trumpocalyspe <mark.jn.kim@gmail.com>
2018-11-15 22:56:56 -0800

    working api_call

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ var openWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 
 var prepareData = function(units) {
   var cityName = $('#city-name').val()
-  console.log(`City: ${cityName}`);
+
   if (cityName && cityName !== "") {
     cityName = cityName.trim()
     getData(openWeatherUrl, cityName, openWeatherAppId, units)
@@ -15,7 +15,6 @@ var prepareData = function(units) {
 
 $(document).ready(function() {
   $('.btn-metric').click(function() {
-    // console.log('yes, great!')
     prepareData('metric')
   })
   $('.btn-imperial').click(function() {
@@ -23,15 +22,28 @@ $(document).ready(function() {
   })
 })
 
+function getData(url, cityName, appId, units) {
+  var request = $.ajax({
+    url: url,
+    dataType: "jsonp",
+    data: { q: cityName, appid: appId, units: units },
+    jsonpCallback: "fetchData",
+    type: "GET"
+  }).fail(function(error){
+    console.error(error)
+    alert('Error sending request')
+  })
+}
+
 function fetchData(forecast) {
   console.log(forecast);
   var html= '',
       cityName = forecast.city.name,
       country = forecast.city.country
 
-    html += '<h3> Weather Forecast for ' + cityName + ', ' + country + '</h3>';
+    // html += '<h3> Weather Forecast for ' + cityName + ', ' + country + '</h3>';
     forecast.list.forEach(function(forecastEntry, index, list) {
-      html += '<p>' + forecastEntry.dt_txt + ': ' + forecastEntry.main.temp + '</p>'
+      html += '<td>' + forecastEntry.dt_txt + ': ' + forecastEntry.main.temp + '</td>'
     })
 
     $('#log').html(html)
